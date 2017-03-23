cf95e1748df815a93131bda4e75973cbe3219f76
徐畅 <xuchang@shtruecolor.com>
2017-03-23 11:02:42 +0800

    first commit

---
+++ Ad/InterstitialExample/app/src/main/java/com/truecolor/interstitialexample/MyApplication.java
@@ -0,0 +1,19 @@
+package com.truecolor.interstitialexample;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+public class MyApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
